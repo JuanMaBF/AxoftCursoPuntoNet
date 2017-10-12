c8564c921bed6cfb320777a950ea3ccc7b283e35
Fafa <juanmanuelbf@gmail.com>
2017-10-12 11:48:00 -0300

    Demo de delegados

---
+++ NET Avanzado/Demos/WindowsTestEvent/Impresora.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WindowsTestEvent
+{
+    public class Impresora
+    {
+        //declaración del evento
+        public event EventHandler ImpresionOK;
+
+        private string _texto;
+        public Impresora(string texto)
+        {
+            this._texto = texto;
+        }
+
+        public void Imprimir()
+        {
+           // Console.Write(this._texto);
+            //invocación del evento
+            if(ImpresionOK!=null)
+            { ImpresionOK(_texto, null);}
+            
+
+        }
+
+    }
+}
