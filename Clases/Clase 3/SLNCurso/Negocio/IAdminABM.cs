942782bd799c29fd2fcd70e4cacf57c4529ba2a0
Fafa <juanmanuelbf@gmail.com>
2017-10-17 12:25:20 -0300

    0

---
+++ Clases/Clase 3/SLNCurso/Negocio/IAdminABM.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections.Generic;
+
+namespace Negocio
+{
+    public interface IAdminABM<T, I>
+    {
+        void Agregar(T profesor);
+        void Modificar(T profesor);
+        void Eliminar(T profesor);
+        List<T> TraerTodos();
+        List<T> TraerTodos(I especialidad);
+    }
+}
